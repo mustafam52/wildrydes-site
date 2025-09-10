e8e8838a32bf857e5999fc557fa4774f42006581
mustafam52 <mustafa.manaf52@gmail.com>
2025-09-10 15:44:10 +1000

    Update config.js
    new pool created-updated
    added secret id

--- js/config.js
+++ js/config.js
@@ -1,11 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_r8FHinWp0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5ka3jh20oorvfsjuuonp3evmh5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_fjLT1I0Ud', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6s505ohvro7uggj2ij3kg1bo49', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
+        clientSecret: '1gm98h94i0529cs3skgulk50fot40fpfv123l99k3v2vdvflcesn'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
