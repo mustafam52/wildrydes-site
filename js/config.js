33b85998439508f211a581691d4d8d570a88d7dd
mustafam52 <mustafa.manaf52@gmail.com>
2025-09-10 15:18:14 +1000

    Update config.js
    updated config with client Id and userpool id and region

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_r8FHinWp0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5ka3jh20oorvfsjuuonp3evmh5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
